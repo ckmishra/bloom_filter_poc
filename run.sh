d9ad21f8d94f6564aaac56a45d0423cdc5967876
Mishra, Chandan <chandan.mishra@move.com>
2019-07-08 22:30:16 -0700

    Fixed run.sh

--- run.sh
+++ run.sh
@@ -4,11 +4,11 @@ arg2="$2"
 if [ ! -n "$arg1" ] || [ ! -n "$arg2" ]
 then
 arg1="./resources/BlackFriday.txt"
-arg2="./resources/test_BlackFriday.txt"
+arg2="./resources/test_all.txt"
 fi
 
 javac -d ./bin ./src/com/bloomfilter/BloomFilter.java
 javac -d ./bin ./src/com/bloomfilter/BigDataStore.java -cp "./bin:./ext/*"
 javac -d ./bin ./test/com/bloomfilter/*.java -cp "./bin:./ext/*"
-#java  -cp "./bin:./ext/*"  com.bloomfilter.BloomFilterTest
+java  -cp "./bin:./ext/*"  com.bloomfilter.BloomFilterTest
 java -cp "./bin:./ext/*" com.bloomfilter.BigDataStoreTest $arg1 $arg2
